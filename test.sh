2379669c7828d9e4a19e9ded62a3550f2472d01d
Nicolas Malbran <nmalbran@gmail.com>
2011-10-27 22:57:26 -0300

    Primer Bot, fin de los tutoriales

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+op=$1
+map=$2
+turns=$3
+
+python tools/playgame.py "python MyBot.py" "python tools/sample_bots/python/$op" --map_file $map --log_dir game_logs --turns $turns --player_seed 7 --verbose -e
