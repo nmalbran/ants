b2612cea0f451c8e4fbd781973e8927e2d085e36
Nicolas Malbran <nmalbran@gmail.com>
2011-11-17 03:20:37 -0300

    added WaveFrontMap for exploring and atakink

---
+++ WaveFrontMap.py
@@ -0,0 +1,89 @@
+from collections import deque
+
+
+class WaveFrontMap(object):
+
+    def __init__(self, rows, cols, origin, view_radius2):
+        self.rows = rows
+        self.cols = cols
+        self.origin = origin
+
+        self.map = [[-100]*cols for row in range(rows)]
+        self.water = set()
+        self.visited = set()
+        self.known_locs = set()
+
+        self.next_loc = deque()
+        self.next_loc.append((origin, 0))
+
+        def get_pre_radius(rad):
+            offsets = []
+            mx = rad
+            rad2 = rad**2
+            for d_row in range(-mx,mx+1):
+                for d_col in range(-mx,mx+1):
+                    d = d_row**2 + d_col**2
+                    if d <= rad2:
+                        offsets.append((
+                            d_row%self.rows-self.rows,
+                            d_col%self.cols-self.cols
+                        ))
+            return offsets
+
+        self.visible_area = get_pre_radius(int(sqrt(view_radius2)))
+
+    def neighborhood(self, loc):
+        row, col = loc
+        neighborhood = set([((row+1)%self.rows, (col+1)%self.cols),
+                        ((row+1)%self.rows, (col-1)%self.cols),
+                        ((row-1)%self.rows, (col+1)%self.cols),
+                        ((row-1)%self.rows, (col-1)%self.cols) ])
+        return neighborhood  - self.water
+
+    def get_radius(self, loc, area):
+        a_row, a_col = loc
+        return set([ ((a_row+v_row)%self.rows, (a_col+v_col)%self.cols) for v_row, v_col in area])
+
+
+    def update(self, ants):
+        rad = self.get_radius
+        va = self.visible_area
+
+        for loc in ants.my_ants():
+            self.known_locs.update(rad(loc, va))
+
+        self.water.update(ants.water())
+        self.wave_expand()
+
+
+    def wave_expand(self):
+        pending_locs = deque()
+
+        while len(self.next_loc) > 0:
+            loc, val = self.next_loc.popleft()
+            if loc in self.known_locs:
+
+                childs = self.neighborhood(loc)
+                for child in childs:
+                    if child not in self.visited:
+                        visited.add(child)
+                        c_row, c_col = child
+                        self.map[c_row][c_col] = val + 1
+                        self.next_loc.append((child, val+1))
+
+            else:
+                pending_locs.append((loc, val))
+
+        self.next_loc = pending_locs
+
+
+    def lower_locs(self,loc):
+        neighborhood = self.neighborhood(loc)
+        val_loc = [ (self.map[row][col], (row, col)) for row, col in neighborhood]
+        val_loc.sort()
+        return val_loc
+
+    def higher_locs(self,loc):
+        lowers = self.lower_locs(loc)
+        lowers.reverse()
+        return lowers
